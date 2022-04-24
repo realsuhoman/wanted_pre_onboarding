4de231134e9f93a181bee9a33be72dbb5e2af212
pepperoni <sinsuho5760@gmail.com>
2022-04-24 22:26:41 +0900

    원티드 프리온보딩 프론트엔드 코스 과제

---
+++ src/components/tab.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Tab = () => {
+  const menu = ['감자', '고구마', '카레라이스'];
+  const [TabClicked, setTabClicked] = useState(0);
+
+  return (
+    <TabContainer>
+      <Wrapper>
+        {menu.map((menu, index) => {
+          return (
+            <TabButton
+              key={menu}
+              id={index}
+              onClick={() => {
+                setTabClicked(index);
+              }}
+              isClicked={TabClicked === index ? true : false}
+            >
+              {menu}
+            </TabButton>
+          );
+        })}
+      </Wrapper>
+      <TabActiveBar>
+        <ActiveLine activeLine={TabClicked} />
+      </TabActiveBar>
+    </TabContainer>
+  );
+};
+
+const TabContainer = styled.div`
+  margin: 10px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 450px;
+  height: 40px;
+`;
+
+const TabButton = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 150px;
+  height: 40px;
+  text-align: center;
+  font-size: 0.9rem;
+  font-weight: 600;
+  cursor: pointer;
+  color: ${(props) => (props.isClicked ? 'black' : 'lightgrey')};
+`;
+
+const TabActiveBar = styled.div`
+  width: 450px;
+  height: 3px;
+  background-color: lightgrey;
+`;
+
+const ActiveLine = styled.div`
+  width: 150px;
+  height: 3px;
+  background-color: blue;
+  transition: all 0.4s ease;
+  transform: translateX(calc(100% * ${(props) => props.activeLine}));
+`;
+
+export default Tab;
---
+++ src/components/toggle.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Toggle = () => {
+  const [clicked, setClicked] = useState(false);
+
+  return (
+    <Wrapper>
+      <Active click={clicked}></Active>
+      <Btn1 click={clicked} onClick={() => setClicked(false)}>
+        기본
+      </Btn1>
+      <Btn1 click={clicked} onClick={() => setClicked(true)}>
+        상세
+      </Btn1>
+    </Wrapper>
+  );
+};
+
+const Active = styled.div`
+  position: absolute;
+  width: 200px;
+  height: 50px;
+  border-radius: 100px;
+  background-color: white;
+  transition: all 0.4s ease;
+  transform: ${(props) => (props.click ? 'translateX(100%)' : 'translateX(0%)')};
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  padding: 3px;
+  margin: 10px;
+  background-color: #ebebeb;
+  width: 400px;
+  height: 50px;
+  border-radius: 100px;
+  z-index: 1;
+`;
+
+const Btn1 = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  cursor: pointer;
+  font-size: 1.1rem;
+  font-weight: bold;
+  z-index: 3;
+`;
+
+export default Toggle;
